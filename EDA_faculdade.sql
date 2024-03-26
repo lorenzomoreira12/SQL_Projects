93247416dffc3d29cc39098cee9a31de2b2d41cd
Lorenzo Moreira <155455706+lorenzomoreira12@users.noreply.github.com>
2024-03-26 12:07:29 -0300

    EDA_faculdade.sql

--- EDA_faculdade.sql
+++ EDA_faculdade.sql
@@ -2,13 +2,13 @@
 SELECT COUNT(idCurso) FROM curso;
 
 -- quais disciplinas sao ofertadas?--
-select nome from disciplina;
+SELECT nome FROM disciplina;
 
 -- em que cursos os estudantes estao matriculados?--
 SELECT c.nome AS nome_curso, a.nome AS nome_aluno
 FROM curso c
-INNER JOIN alunocurso ac ON c.idCurso = ac.idCurso
-INNER JOIN aluno a ON ac.idAluno = a.idAluno
+  INNER JOIN alunocurso ac ON c.idCurso = ac.idCurso
+  INNER JOIN aluno a ON ac.idAluno = a.idAluno
 ORDER BY nome_aluno DESC;
 
 -- qual a media das notas das disciplinas de todos os cursos da faculdade? --
@@ -19,5 +19,5 @@ GROUP BY idDisciplina;
 -- quantos alunos estão matriculados por curso?--
 SELECT c.nome AS nome_curso, COUNT(ac.idAluno) AS quantidade_alunos
 FROM curso c
-LEFT JOIN alunocurso ac ON c.idCurso = ac.idCurso
+  LEFT JOIN alunocurso ac ON c.idCurso = ac.idCurso
 GROUP BY c.nome;
